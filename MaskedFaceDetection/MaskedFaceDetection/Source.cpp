f7d897eccdca327ec54c305f55ea9ee1c5d6e659
AlisterMartin <b8023382@my.shu.ac.uk>
2021-04-26 15:07:48 +0100

    Add camera support

--- MaskedFaceDetection/MaskedFaceDetection/Source.cpp
+++ MaskedFaceDetection/MaskedFaceDetection/Source.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 using namespace cv;
-void detectAndDisplay(Mat frame);
+void detectAndDisplay(Mat);
 void test();
-void detect();
+void detect(int);
 
 CascadeClassifier face_cascade;
 
@@ -14,9 +14,9 @@ int main(int argc, const char** argv)
 {
     CommandLineParser parser(argc, argv,
         "{help h ?  |       |                           }"
-        "{test      |0      |Enable test mode (0/1)     }"
+        "{test      |0      |Enable test mode (0/1).    }"
         "{camera    |0      |Camera number.             }"
-        "{audio     |1      |Enable audio (0/1)         }");
+        "{audio     |1      |Enable audio (0/1).        }");
     parser.about("MaskedFaceDetection v0.1");
     String face_cascade_name = samples::findFile("maskedFaceCascade.xml");
     if (!face_cascade.load(face_cascade_name))
@@ -29,7 +29,7 @@ int main(int argc, const char** argv)
         test();
     }
     else {
-        detect();
+        detect(parser.get<int>("camera"));
     }
     
     return 0;
@@ -50,38 +50,25 @@ void test() {
     }
 }
 
-void detect() {
-    /*
-    int camera_device = parser.get<int>("camera");
+void detect(int camera_device) {
     VideoCapture capture;
-    //-- 2. Read the video stream
     capture.open(camera_device);
     if (!capture.isOpened())
     {
-        cout << "--(!)Error opening video capture\n";
-        return -1;
+        cout << "ERROR STARTING CAPTURE" << endl;
+        return;
     }
 
     Mat frame;
     while (capture.read(frame))
     {
-    */
-
-    std::string path = "A:\\Documents\\FinalYearProject\\FMLD-main\\test-images\\images";
-    for (const auto& entry : filesystem::directory_iterator(path)) {
-        while (waitKey(10) != 27) {
-
-            Mat img = imread(entry.path().u8string());
-            if (img.empty())
-            {
-                cout << "--(!) No image -- Break!\n";
-            }
-            //-- 3. Apply the classifier to the frame
-            detectAndDisplay(img);
-
+        if (frame.empty())
+        {
+            cout << "CAMERA UNRESPONSIVE" << endl;
         }
+        detectAndDisplay(frame);
+
     }
-    //}
 }
 
 void detectAndDisplay(Mat frame)
