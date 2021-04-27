7a2e0dd885dcfbad8db8a3e28ac8304b005acb6f
AlisterMartin <b8023382@my.shu.ac.uk>
2021-04-27 09:42:33 +0100

    Add "Enhanced Cadcade" and fix commandline iterpretation.

--- MaskedFaceDetection/MaskedFaceDetection/Source.cpp
+++ MaskedFaceDetection/MaskedFaceDetection/Source.cpp
@@ -14,7 +14,7 @@ int main(int argc, const char** argv)
 {
     CommandLineParser parser(argc, argv,
         "{help h ?  |       |                           }"
-        "{test      |0      |Enable test mode (0/1).    }"
+        "{test      |       |Enable test mode.          }"
         "{camera    |0      |Camera number.             }"
         "{audio     |1      |Enable audio (0/1).        }");
     parser.about("MaskedFaceDetection v0.1");
@@ -22,14 +22,15 @@ int main(int argc, const char** argv)
         parser.printMessage();
         return 0;
     }
-    String face_cascade_name = samples::findFile("maskedFaceCascade.xml");
+    String face_cascade_name = samples::findFile("EnhancedCascade.xml");
     if (!face_cascade.load(face_cascade_name))
     {
         cout << "CASCADE LOAD ERROR" << endl;
         return -1;
     };
     
-    if (parser.get<int>("test")) {
+    
+    if (parser.has("test")) {
         test();
     }
     else {
@@ -59,7 +60,7 @@ void detect(int camera_device) {
     capture.open(camera_device);
     if (!capture.isOpened())
     {
-        cout << "ERROR STARTING CAPTURE" << endl;
+        cout << "ERROR STARTING CAPTURE (CHECK CAMERA)" << endl;
         return;
     }
 
@@ -80,7 +81,6 @@ void detectAndDisplay(Mat frame)
     Mat frame_gray;
     cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
     equalizeHist(frame_gray, frame_gray);
-    //-- Detect faces
     std::vector<Rect> faces;
     face_cascade.detectMultiScale(frame_gray, faces);
     for (size_t i = 0; i < faces.size(); i++)
@@ -89,7 +89,6 @@ void detectAndDisplay(Mat frame)
         rectangle(frame, faces.at(i), Scalar(255, 0, 255), 4);
         Mat faceROI = frame_gray(faces[i]);
     }
-    //-- Show what you got
     namedWindow("Detection", CV_WINDOW_NORMAL);
     imshow("Detection", frame);
 }
